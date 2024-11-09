051f59e7a0a103ada98102deb2840d6a09687d6a
vijaykumarnavale <navalevijay85@gmail.com>
2024-11-09 11:07:27 +0530

    fsd completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './login.js';
+// App.js
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './login.js'
+import Dashboard from './dashboard.js';
+
 function App() {
-  return (
-   <>
-  <Login/>
-   </>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+    );
 }
 
 export default App;
--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,19 +1,21 @@
-
+// Login.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function Login({ onLoginSuccess }) {
+function Login() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://localhost:5000/api/login', { username, password });
             alert(response.data.message);
-            onLoginSuccess();
+            navigate('/dashboard');
         } catch (error) {
-            alert(error.response.data.message);
+            alert(error.response.data.message || "Login failed");
         }
     };
 
